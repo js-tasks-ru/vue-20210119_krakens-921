0334cf2e1c7e35392d9ca30e8c81730a7e643abb
m.bobovikov <m.bobovikov@involta.ru>
2021-03-18 21:31:32 +0300

    NodesPagination solution

--- 06-deep-vue/07-NodesPagination/components/NodesPagination.vue
+++ 06-deep-vue/07-NodesPagination/components/NodesPagination.vue
@@ -1,6 +1,25 @@
 <script>
 export default {
   name: 'NodesPagination',
+  props: {
+    page: {
+      type: Number,
+      default: 1,
+    },
+    perPage: {
+      type: Number,
+      default: null,
+    },
+  },
+  render(createElement) {
+    if (this.perPage === null) return createElement('div', this.$slots.default);
+
+    const lastNodeIndex = this.page * this.perPage;
+    const firstNodeIndex = lastNodeIndex - this.perPage;
+    const nodeList = this.$slots.default.slice(firstNodeIndex, lastNodeIndex);
+
+    return createElement('div', nodeList);
+  },
 };
 </script>
 
